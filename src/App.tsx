ec738c54b65fa635219f54a28ef27f02a18d5f96
Warren Hong <hwhong1129@gmail.com>
2022-07-19 22:11:03 -0700

    worked on the first try

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import styles from "./app.module.css";
 // import { useCanvasObject } from "./hook";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [startPoint, setStartPoint] = useState({ x: 0, y: 0 });
+  const [endPoint, setEndPoint] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -20,18 +23,44 @@ function App() {
   }, []);
 
   const onMouseDown = (event: any) => {
+    setIsMouseDown(true);
+    setStartPoint({ x: event.pageX, y: event.pageY });
+  };
+
+  const onMouseMove = (event: any) => {
+    event.preventDefault();
+    if (isMouseDown) {
+      var endPoint = {
+        x: event.pageX,
+        y: event.pageY,
+      };
+
+      setEndPoint(endPoint);
+    }
+  };
+
+  const onMouseUp = () => {
+    const left = Math.min(startPoint.x, endPoint.x);
+    const top = Math.min(startPoint.y, endPoint.y);
+    const width = Math.abs(startPoint.x - endPoint.x);
+    const height = Math.abs(startPoint.y - endPoint.y);
+
     const canvas = canvasRef.current;
     const ctx = canvas?.getContext("2d");
     if (canvas && ctx) {
       ctx.fillStyle = "#FF0000";
-      ctx.fillRect(event.screenX, event.screenY, 5, 5);
+      ctx.fillRect(left, top, width, height);
     }
-    console.log(event);
   };
 
   return (
     <div className={styles.root}>
-      <canvas ref={canvasRef} onMouseDown={onMouseDown} />
+      <canvas
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+      />
     </div>
   );
 }
